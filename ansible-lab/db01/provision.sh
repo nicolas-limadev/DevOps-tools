26f22f9cccaee26d1def327bf539fd525bd2cf49
nicolas-limadev <nicolasbasto76@gmail.com>
2023-06-06 23:58:18 -0300

    jenkinsfile-update

--- ansible-lab/db01/provision.sh
+++ ansible-lab/db01/provision.sh
@@ -1,4 +1,3 @@
 #/bin/sh
 cat << EOT >> /home/vagrant/.ssh/authorized_keys
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDydnlkJz5sDvfcDC0lenyzZo1/s5oacbq3liDZV1I8+/kS2YlfaC7cP+2fcw2b70Ent6FCU9v0yxZzZBwe/sr9V5IFHksYhKUWXWtax9Iy5CVZ9YU1nUaOzHSBHZYekSTNvsQR5VWJgepqnqTx610tpTkNiGcG0H0fOlSg8VEIlZdDb+QGqFdk0aqra5zlIRWIscfsDLC+E5Dvilih+1ySK9sZB3RW9zDToiZi/PdqD5ExMq1yCGbtAckgboD3kvMJYMac4BRh1fC3JgbrLP4rmS6Xm1XtlaOfTTQCttM2//9v4jko79AlfMHB1ysLbStPxzO8tyRv/aP02/5DVGv vagrant@control-node
 EOT

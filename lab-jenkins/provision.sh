54f1612a81888700a175ab0af48a32a7529235a5
nicolas-limadev <nicolasbasto76@gmail.com>
2023-06-06 21:27:40 -0300

    lab-jenkins

--- lab-jenkins/provision.sh
+++ lab-jenkins/provision.sh
@@ -1,23 +1,33 @@
 #/bin/bash
-sudo apt install epel-release -y
-sudo apt install -y wget curl
-wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update
-sudo apt install openjdk-11-jdk -y
-sudo apt install -y jenkins
+yum install epel-release -y
+yum install wget git -y
+sudo wget --no-check-certificate -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+yum install java-11-openjdk-devel -y
+yum install jenkins -y
+systemctl daemon-reload
+service jenkins start
 
-# Instalacao Docker e Docker compose
-sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt install -y docker-ce docker-ce-cli containerd.io -y
+###instalacao do docker e docker compose
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli  containerd.io -y
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
 systemctl daemon-reload
 systemctl restart docker
+usermod -aG docker jenkins
+
+#instalacao do sonar scanner
+
+yum install wget unzip -y
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.6.2.2472-linux.zip
+unzip sonar-scanner-cli-4.6.2.2472-linux.zip -d /opt/
+mv /opt/sonar-scanner-4.6.2.2472-linux /opt/sonar-scanner
+chown -R jenkins:jenkins /opt/sonar-scanner
+echo 'export PATH=$PATH:/opt/sonar-scanner/bin' | sudo tee -a /etc/profile
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+sudo yum install nodejs -y

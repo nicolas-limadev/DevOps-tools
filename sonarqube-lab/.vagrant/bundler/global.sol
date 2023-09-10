08871ed86200b4a99fd0504d6a3085b58078c568
nicolas-limadev <nicolasbasto76@gmail.com>
2023-09-10 04:58:29 -0300

    finaly-labs

--- sonarqube-lab/.vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.31.0"]]],"checksum":"50cabdd1d2aa3beed69d011439135b384910e63ac1284132ab17395559745a77","vagrant_version":"2.3.7"}
